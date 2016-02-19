05f4a89e303fe7b7e94a88a5c9edfea4ac9d6fbe
Brianna Popson <bpopson@gmail.com>
2016-02-19 09:20:01 -0800

    adjust first spec to encompass entire desired function and still pass

--- src/WordRepeatCounter.php
+++ src/WordRepeatCounter.php
@@ -2,13 +2,21 @@
 
 class WordRepeatCounter
 {
-    function stringFormat($string_input)
+    function stringWordCount($string_input, $word_to_count)
     {
         $string_strip = array("?", ",", ".", "!", ";", ":", "/", "$", "#", "%", "&", "(", ")");
         $format_string = str_replace($string_strip, "", $string_input);
         $format_string = strtolower($format_string);
         $format_string = explode(" ", $format_string);
-        return $format_string;
+        $word_to_count = strtolower($word_to_count);
+        $wordCount = 0;
+
+        foreach ($format_string as $word) {
+          if ($word == $word_to_count) {
+            $wordCount ++;
+          }
+        }
+        return $wordCount;
     }
 }
 
--- tests/WordRepeatCounterTest.php
+++ tests/WordRepeatCounterTest.php
@@ -5,18 +5,20 @@
     class WordRepeatCounterTest extends PHPUnit_Framework_TestCase
     {
 
-        function test_stringFormat_initial()
+        function test_stringFormat_count() //this includes formatting the string and input word so that the function can easily seek out the desired word.
         {
             //Arrange
             $test_WordRepeatCounter = new WordRepeatCounter;
-            $string_input = "How now brown cow? How are you?!";
+            $string_input = "A hydrochloric acid solution of 4% has a smaller reaction rate than a solution of 16%, which therefore proves that a smaller amount of concentration will result in a lower rate of reaction. This concludes that a solution with a smaller amount of concentration has a lower rate of reaction.";
+            $word_to_count = "a";
 
             //Act
-            $result = $test_WordRepeatCounter->stringFormat($string_input);
+            $result = $test_WordRepeatCounter->stringWordCount($string_input, $word_to_count);
 
             //Assert
-            $this->assertEquals(array('how', 'now', 'brown', 'cow', 'how', 'are', 'you'), $result);
+            $this->assertEquals(8, $result);
         }
+
     }
 
 ?>
