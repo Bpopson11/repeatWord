929b677b0bf60f1b9870aaf4bd2e8ac14de19db9
Brianna Popson <student@epicodus.com>
2016-02-24 12:20:54 -0800

    simplify and create more spec tests

--- tests/WordRepeatCounterTest.php
+++ tests/WordRepeatCounterTest.php
@@ -5,18 +5,32 @@
     class WordRepeatCounterTest extends PHPUnit_Framework_TestCase
     {
 
-        function test_stringFormat_count() //this includes formatting the string and input word so that the function can easily seek out the desired word.
+        function test_stringFormat_1letter() //this includes formatting the string and input word so that the function can easily seek out the desired word.
         {
             //Arrange
             $test_WordRepeatCounter = new WordRepeatCounter;
-            $string_input = "A hydrochloric acid solution of 4% has a smaller reaction rate than a solution of 16%, which therefore proves that a smaller amount of concentration will result in a lower rate of reaction. This concludes that a solution with a smaller amount of concentration has a lower rate of reaction.";
+            $string_input = "a";
             $word_to_count = "a";
 
             //Act
             $result = $test_WordRepeatCounter->stringWordCount($string_input, $word_to_count);
 
             //Assert
-            $this->assertEquals(8, $result);
+            $this->assertEquals(1, $result);
+        }
+
+        function test_stringFormat_2letter() //this includes formatting the string and input word so that the function can easily seek out the desired word.
+        {
+            //Arrange
+            $test_WordRepeatCounter = new WordRepeatCounter;
+            $string_input = "a";
+            $word_to_count = "b";
+
+            //Act
+            $result = $test_WordRepeatCounter->stringWordCount($string_input, $word_to_count);
+
+            //Assert
+            $this->assertEquals(0, $result);
         }
 
 
